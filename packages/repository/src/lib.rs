39571a5e42a4dad33b944a72bbc63ec8a08564f1
sadness_ojisan <sadness.ojisan@gmail.com>
2022-07-15 18:47:10 +0900

    Add users fethcer

--- packages/repository/src/lib.rs
+++ packages/repository/src/lib.rs
@@ -1,9 +1,25 @@
-use domain::{TheaterData};
-#![feature(once_cell)]
-use std::lazy::OnceCell;
-
-static Cella = OnceCell::new();
-
+use domain::TheaterData;
+use futures_util::TryStreamExt;
+use sqlx::mysql::MySqlPoolOptions;
 struct Repository {
-    data: TheaterData
+    data: TheaterData,
+}
+
+impl Repository {
+    pub async fn getUsers() -> Result<(), sqlx::Error> {
+        let pool = MySqlPoolOptions::new()
+            .max_connections(5)
+            .connect("mysql://root:root_password@localhost:3306/rust_ddd_practice")
+            .await?;
+
+        // Make a simple query to return the given parameter (use a question mark `?` instead of `$1` for MySQL)
+        let mut rows = sqlx::query("SELECT * from user").fetch(&pool);
+
+        while let Some(row) = rows.try_next().await? {
+            // map the row into a user-defined domain type
+            dbg!(row);
+        }
+
+        Ok(())
+    }
 }
