fc7a4655b3a583f1984acef59a030f43b1db9738
sadness_ojisan <sadness.ojisan@gmail.com>
2022-07-15 17:25:47 +0900

    Run mysql

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,2 @@
-FROM mysql
-
-EXPOSE 3306
-
-ADD ./my.cnf /etc/mysql/conf.d/my.cnf
-COPY ./init.sql /docker-entrypoint-initdb.d/init.sql
-
-ENV MYSQL_ROOT_PASSWORD rust-ddd-practice
-ENV MYSQL_ALLOW_EMPTY_PASSWORD local-pass
-ENV MYSQL_RANDOM_ROOT_PASSWORD local-pass
-
-CMD ["mysqld"]
+FROM mysql:8.0
+ENV LANG ja_JP.UTF-8
---
+++ initdb.d/init.sql
@@ -0,0 +1,22 @@
+DROP SCHEMA IF EXISTS rust_ddd_practice;
+CREATE SCHEMA rust_ddd_practice;
+USE rust_ddd_practice;
+
+DROP TABLE IF EXISTS user;
+
+CREATE TABLE user(
+  id           INT(10),
+  name     VARCHAR(40),
+  age          INT(10),
+--   0,1,2,9
+  gender          INT(10)
+);
+
+INSERT INTO user (id, name, age, gender) VALUES (1, "Nagaoka", 18, 1);
+
+--  CREATE TABLE IF NOT EXISTS `rust_ddd_practice_schema`.`users` (
+--    `id` INT NOT NULL COMMENT 'ユーザID',
+--    `name` VARCHAR(45) NOT NULL COMMENT 'ユーザ名',
+--    `create_date` DATETIME NULL,
+--    `update_date` DATETIME NULL,
+--    PRIMARY KEY (`user_id`))
